3df0b1b3d50a38cc0c8d3c7cb0e922469e7e16c6
Laurent Peuch <cortex@worlddomination.be>
2012-01-20 23:20:25 +0100

    [mod] intermediare patch, get feeds and empty function to get handlers for them

--- feedpopulator/feedpopulator.py
+++ feedpopulator/feedpopulator.py
@@ -10,7 +10,17 @@ def check_config_dir():
     if not exists(config.path):
         makedirs(config.path)
 
+def get_feeds():
+    return filter(None, open(config.path + "urls", "r").read().split("\n"))
+
+def get_feed_handler(feed):
+    return None
+
 def run():
     check_config_dir()
     if not exists(config.path + "urls"):
         print >>sys.stderr, "Error: you don't have any ~/.config/feedpopulator/urls"
+
+    for feed in get_feeds():
+        if get_feed_handler(feed) is None:
+            print >>sys.stderr, "Error: no known handler for %s" % feed
